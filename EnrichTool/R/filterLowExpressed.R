9bedb7d86bc1692bbadc1afa3b5bde3fd64fe94d
a20anena <aneeqanadeem555@gmail.com>
2025-12-07 20:08:34 +0100

    Update filterLowExpressed.R

--- EnrichTool/R/filterLowExpressed.R
+++ EnrichTool/R/filterLowExpressed.R
@@ -1,6 +1,10 @@
 filterLowExpressed <- function(count_data, min_count = 10) {
-  # Remove low-expressed genes
+
+  # remove genes that don't have enough counts
   keep <- rowSums(count_data) >= min_count
+  
+  # keep only the rows that passed the filter
   filtered_data <- count_data[keep, ]
+  
   return(filtered_data)
 }
